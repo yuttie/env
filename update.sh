4a50d98d1293b43db70c73af1b62ed5524f0b853
Yuta Taniguchi <yuta.taniguchi.y.t@gmail.com>
2019-08-26 15:26:52 +0900

    Improve the update script so that it can recognize targets to update

--- update.sh
+++ update.sh
@@ -1,60 +1,83 @@
 #!/bin/sh
 
+targets=${@:-tex python nodejs julia rust zsh}
+
+is_specified() {
+  for t in $targets; do
+    if [ "$1" = "$t" ]; then
+      return 0
+    fi
+  done
+  return 1
+}
+
 # TeX
-for TLMGR in /usr/local/texlive/2019/bin/{x86_64-linux,x86_64-darwin}/tlmgr; do
-  if [ -f $TLMGR -a -x $TLMGR ]; then
-    break
-  else
-    TLMGR=''
+if is_specified tex; then
+  for TLMGR in /usr/local/texlive/2019/bin/{x86_64-linux,x86_64-darwin}/tlmgr; do
+    if [ -f $TLMGR -a -x $TLMGR ]; then
+      break
+    else
+      TLMGR=''
+    fi
+  done
+  if [ -n "$TLMGR" ]; then
+    echo "Found \`tlmgr' at $TLMGR"
+    sudo sh -c "$TLMGR update --self; $TLMGR update --all"
   fi
-done
-if [ -n "$TLMGR" ]; then
-  echo "Found \`tlmgr' at $TLMGR"
-  sudo sh -c "$TLMGR update --self; $TLMGR update --all"
 fi
 
 # Python
-PIP3_PACKAGES='pip pipenv awscli pynvim i3-py'
-if which pip3 >/dev/null 2>&1; then
-  pip3 install --user --upgrade $PIP3_PACKAGES
+if is_specified python; then
+  PIP3_PACKAGES='pip pipenv awscli pynvim i3-py'
+  if which pip3 >/dev/null 2>&1; then
+    pip3 install --user --upgrade $PIP3_PACKAGES
+  fi
 fi
 
 # Node.js
-NPM_PACKAGES='npm vsce heroku neovim svgo live-server backslide'
-if which npm >/dev/null 2>&1; then
-  for p in $NPM_PACKAGES; do
-    npm install -g $p
-  done
+if is_specified nodejs; then
+  NPM_PACKAGES='npm vsce heroku neovim svgo live-server backslide'
+  if which npm >/dev/null 2>&1; then
+    for p in $NPM_PACKAGES; do
+      npm install -g $p
+    done
+  fi
 fi
 
 # Julia
-JULIA_PACKAGES='Gadfly CSV Cairo Fontconfig'
-if which julia >/dev/null 2>&1; then
-  for p in $JULIA_PACKAGES; do
-    julia --eval "using Pkg; Pkg.add(\"$p\")"
-  done
+if is_specified julia; then
+  JULIA_PACKAGES='Gadfly CSV Cairo Fontconfig'
+  if which julia >/dev/null 2>&1; then
+    for p in $JULIA_PACKAGES; do
+      julia --eval "using Pkg; Pkg.add(\"$p\")"
+    done
+  fi
 fi
 
 # Rust
-if which rustup >/dev/null 2>&1; then
-  rustup self update
-  rustup update
-  rustup component add rls rust-analysis rust-src
-fi
+if is_specified rust; then
+  if which rustup >/dev/null 2>&1; then
+    rustup self update
+    rustup update
+    rustup component add rls rust-analysis rust-src
+  fi
 
-CARGO_PACKAGES_FOR_STABLE='ripgrep fd xsv exa bat oxipng'
-CARGO_PACKAGES_FOR_NIGHTLY='racer'
-if which cargo >/dev/null 2>&1; then
-  cargo uninstall $CARGO_PACKAGES_FOR_STABLE
-  cargo install $CARGO_PACKAGES_FOR_STABLE
+  CARGO_PACKAGES_FOR_STABLE='ripgrep fd xsv exa bat oxipng'
+  CARGO_PACKAGES_FOR_NIGHTLY='racer'
+  if which cargo >/dev/null 2>&1; then
+    cargo uninstall $CARGO_PACKAGES_FOR_STABLE
+    cargo install $CARGO_PACKAGES_FOR_STABLE
 
-  # Run `rustup toolchain install nightly` in advance
-  cargo +nightly uninstall $CARGO_PACKAGES_FOR_NIGHTLY
-  cargo +nightly install $CARGO_PACKAGES_FOR_NIGHTLY
+    # Run `rustup toolchain install nightly` in advance
+    cargo +nightly uninstall $CARGO_PACKAGES_FOR_NIGHTLY
+    cargo +nightly install $CARGO_PACKAGES_FOR_NIGHTLY
+  fi
 fi
 
 # Zsh
-if which antibody >/dev/null 2>&1; then
-  antibody bundle < ~/zdotdir/.zsh_plugins.txt > ~/.zsh_plugins.sh
-  antibody update
+if is_specified zsh; then
+  if which antibody >/dev/null 2>&1; then
+    antibody bundle < ~/zdotdir/.zsh_plugins.txt > ~/.zsh_plugins.sh
+    antibody update
+  fi
 fi
