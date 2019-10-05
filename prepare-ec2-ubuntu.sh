c5d6042457346870a0f986c25419e5f27e2b9167
Yuta Taniguchi <yuta.taniguchi.y.t@gmail.com>
2019-10-06 01:29:16 +0900

    Add a script for setup EC2 Ubuntu instance
    Not tested yet.

---
+++ prepare-ec2-ubuntu.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+REMOTE=$1
+ssh $REMOTE sudo apt update
+ssh $REMOTE sudo apt upgrade
+ssh $REMOTE sudo apt install neovim tmux python3.7{,-dev} python3-distutils build-essential nodejs
+ssh $REMOTE curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py
+ssh $REMOTE python3.7 get-pip.py --user
+ssh $REMOTE pip3.7 install --user --upgrade pip pipenv pynvim neovim-remote
+ssh $REMOTE mkdir -p ~/.local/bin
+ssh $REMOTE mkdir -p ~/.config
+rsync -avSHAXz ~/.config/coc ~/.config/nvim $REMOTE:.config/
+rsync -avSHAXz ~/dotfiles/{.git{attributes,config,ignore},.npmrc,.tmux.conf} $REMOTE:
+rsync -avSHAXz ~/.local/bin/{antibody,fzf} $REMOTE:.local/bin/
+rsync -avSHAXz ~/{zdotdir,.zlogin,.zlogout,.zprofile,.zshenv,.zshrc,.zfunc,.zsh.d,.zsh_plugins.sh} $REMOTE:
