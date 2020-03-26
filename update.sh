73a140c5f897c64d95842c33454e223ec7708e1d
Yuta Taniguchi <yuta.taniguchi.y.t@gmail.com>
2020-03-26 18:08:36 +0900

    Rename a variable

--- update.sh
+++ update.sh
@@ -39,9 +39,9 @@ fi
 
 # Node.js
 if is_specified nodejs; then
-  NPM_PACKAGES='vsce heroku neovim svgo live-server backslide @vue/cli vuepress'
+  YARN_PACKAGES='vsce heroku neovim svgo live-server backslide @vue/cli vuepress'
   if which yarn >/dev/null 2>&1; then
-    yarn global add $NPM_PACKAGES
+    yarn global add $YARN_PACKAGES
   fi
 fi
 
