6d8419d0f4cf4ef350376d4fd936a64c04703994
Yuta Taniguchi <yuta.taniguchi.y.t@gmail.com>
2022-09-19 20:49:23 +0900

    Remove rls and rust-analysis components

--- update.sh
+++ update.sh
@@ -74,7 +74,7 @@ if is_specified rust; then
   if command -v rustup >/dev/null 2>&1; then
     rustup self update
     rustup update
-    rustup component add rls rust-analysis rust-src clippy rustfmt
+    rustup component add rust-src clippy rustfmt
     rustup target add x86_64-unknown-linux-musl
     rustup toolchain add nightly
   fi
