3f6ceca6bb9c1aa45fc1e95e8a3e6e6b696cc694
Yuta Taniguchi <yuta.taniguchi.y.t@gmail.com>
2024-07-16 16:08:39 +0900

    Don't manually install rust-analyzer, which is now managed by rustup

--- update.sh
+++ update.sh
@@ -107,10 +107,6 @@ if is_specified rust; then
     cargo uninstall texlab
     cargo install --locked --git https://github.com/latex-lsp/texlab.git
   fi
-
-  # Other Rust-related programs
-  curl -L https://github.com/rust-analyzer/rust-analyzer/releases/latest/download/rust-analyzer-x86_64-unknown-linux-gnu.gz | gunzip -c - > ~/.local/bin/rust-analyzer
-  chmod +x ~/.local/bin/rust-analyzer
 fi
 
 # Fish
